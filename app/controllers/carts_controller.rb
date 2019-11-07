5073dfe58a8c4045ee96d04d2a48ade3fc046c75
Cai heng <ianyaheng822@gmail.com>
2019-11-08 10:55:13 +1100

    add the payment style

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -58,27 +58,27 @@ class CartsController < ApplicationController
   end
 
   
-  def update
-    respond_to do |format|
-      if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cart }
-      else
-        format.html { render :edit }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @cart.update(cart_params)
+  #       format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @cart }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @cart.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
 
-  def destroy
-    @cart.destroy if @cart.id == session[:cart_id]
-    session[:cart_id] = nil
-    respond_to do |format|
-      format.html { redirect_to root_path, notice: 'Cart was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @cart.destroy if @cart.id == session[:cart_id]
+  #   session[:cart_id] = nil
+  #   respond_to do |format|
+  #     format.html { redirect_to root_path, notice: 'Cart was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
    
