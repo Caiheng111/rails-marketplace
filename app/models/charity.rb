d433d003ac60eb6a21cc732cd6ff2ca4c351aa0d
Cai heng <ianyaheng822@gmail.com>
2019-11-27 20:39:39 +1100

    amend the delete function for the charity

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,11 +1,11 @@
 class Charity < ApplicationRecord
  
  
-  before_destroy :not_referenced_by_any_line_item
+
   belongs_to :category
   belongs_to :organization
   has_many_attached :pictures
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :summary, :challenges, :solution,  presence: true, length: { minimum: 10, maximum: 1000 }
